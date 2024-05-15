11de1493a3eba30dae4b01252931790c6375877c
Le Huynh Duc <ducmta1997@gmail.com>
2024-05-15 21:31:19 +0700

    first version

---
+++ 3-coffee-shop/frontend/src/environments/environment.ts
@@ -0,0 +1,29 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+// export const environment = {
+//   production: false,
+//   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+//   auth0: {
+//     url: 'gfred', // the auth0 domain prefix
+//     audience: 'coffeeshop', // the audience set for the auth0 app
+//     clientId: 'mdGdcAXUgUg7BrdwyZBFI5EfQG2iUbEI', // the client id generated for the auth0 app
+//     callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
+//   }
+// };
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'dev-bziyhi8qwaekwd3z.us', // the auth0 domain prefix
+    audience: 'image', // the audience set for the auth0 app
+    clientId: 'DVhndO3e5HWlKqD6rzDu6dmGsUayXrT0', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+  }
+};
+
+
+
+// DVhndO3e5HWlKqD6rzDu6dmGsUayXrT0
